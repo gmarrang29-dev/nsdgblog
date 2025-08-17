a2a7c12ebd470f59bc20916d8b0e807bff825fb5
gmarrang29-dev <gmarrang29@pess.cnehs.kr>
2025-08-17 17:03:41 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -7,21 +7,28 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged } from
 // =================================================================
 // 2. 파이어베이스 프로젝트의 비밀 열쇠
 // =================================================================
-const firebaseConfig = {
-  apiKey: "여기에_복사한_apiKey_붙여넣기",
-  authDomain: "여기에_복사한_authDomain_붙여넣기",
-  projectId: "여기에_복사한_projectId_붙여넣기",
-  storageBucket: "여기에_복사한_storageBucket_붙여넣기",
-  messagingSenderId: "여기에_복사한_messagingSenderId_붙여넣기",
-  appId: "여기에_복사한_appId_붙여넣기"
-};
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// =================================================================
-// 3. 파이어베이스 초기 설정 및 도구 준비
-// =================================================================
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider(); // 이제 이 코드가 정상적으로 작동합니다.
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBEMuXpcTWRGj7gQzrTNlTY-yYQm33J36s",
+    authDomain: "nsdgblog-42b3e.firebaseapp.com",
+    projectId: "nsdgblog-42b3e",
+    storageBucket: "nsdgblog-42b3e.firebasestorage.app",
+    messagingSenderId: "540516487909",
+    appId: "1:540516487909:web:1f755b7fcb2935aa2dba0c",
+    measurementId: "G-D2CHDR4QGN"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  const provider = new GoogleAuthProvider(); // 이제 이 코드가 정상적으로 작동합니다.
 
 // =================================================================
 // 4. HTML의 '구글로 로그인' 버튼 가져오기
