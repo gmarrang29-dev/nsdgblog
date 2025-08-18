24654c69c4f680e0788c3a0d3f2f66ee246d1c6e
gmarrang29-dev <gmarrang29@pess.cnehs.kr>
2025-08-18 19:48:22 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-// login.js
 
-import { auth } from './firebase-config.js'; 
-import { GoogleAuthProvider, signInWithPopup, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBEMuXpcTWRGj7gQzrTNlTY-yYQm33J36s",
+    authDomain: "nsdgblog-42b3e.firebaseapp.com",
+    projectId: "nsdgblog-42b3e",
+    storageBucket: "nsdgblog-42b3e.firebasestorage.app",
+    messagingSenderId: "540516487909",
+    appId: "1:540516487909:web:1f755b7fcb2935aa2dba0c",
+    measurementId: "G-D2CHDR4QGN"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
 
 const provider = new GoogleAuthProvider();
 const loginBtn = document.getElementById('loginBtn');
