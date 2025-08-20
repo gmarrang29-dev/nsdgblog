287a4e8bd7c878c4a936f7a2f411b3d6fbbbb632
gmarrang29-dev <gmarrang29@pess.cnehs.kr>
2025-08-20 20:00:08 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,39 +1,24 @@
 import { auth } from './firebase-config.js'; 
-import { GoogleAuthProvider, signInWithPopup, signInAnonymously, onAuthStateChanged } 
-  from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import { GoogleAuthProvider, signInWithPopup, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
 const provider = new GoogleAuthProvider();
+
 const loginBtn = document.getElementById('loginBtn');
-const guestBtn = document.getElementById('guest');
 
 if (loginBtn) {
   loginBtn.addEventListener('click', () => {
     signInWithPopup(auth, provider)
       .then(result => {
-        console.log("구글 로그인 성공:", result.user.displayName);
-        window.location.href = "index.html";
+        console.log("로그인 성공!", result.user.displayName);
       })
       .catch(error => {
-        console.error("구글 로그인 에러:", error);
-      });
-  });
-}
-
-if (guestBtn) {
-  guestBtn.addEventListener('click', () => {
-    signInAnonymously(auth)
-      .then(() => {
-        console.log("게스트 로그인 성공");
-        window.location.href = "index.html";
-      })
-      .catch(error => {
-        console.error("게스트 로그인 에러:", error);
+        console.error("로그인 중 에러 발생:", error);
       });
   });
 }
 
 onAuthStateChanged(auth, user => {
   if (user) {
-    console.log(user.isAnonymous ? "게스트 로그인 상태" : "구글 로그인 상태");
+    window.location.href = "index.html";
   }
 });
